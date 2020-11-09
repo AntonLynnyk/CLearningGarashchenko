957ced1a4ba6ebea9ee66fe10d83711277f7f693
Serhii Harashchenko <device11194@gmail.com>
2020-11-09 23:35:49 +0200

    Added some tasks to Operations_C.

--- Operations_C/main.c
+++ Operations_C/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-enum Value {Five = 5, Seven = 7, Eleven = 11, Thirteen = 13} number;
-
-void FirstMethod(enum Value number)
+void FirstMethod(int number)
 {
+    printf("Here are multiple numbers of your number: ");
+
     for (int i = number; i < 10000; i+=number)
     {
         printf("%d ", i);
     }
+
     printf("\n");
 }
 
@@ -39,17 +40,93 @@ int Compare(int a, int b)
     else return -1;
 }
 
+void MoveBites()
+{
+    unsigned char x = 0b00010010;
+
+    unsigned char n = x << 3;
+
+    unsigned char m = n >> 2;
+
+    printf("Number before the move is %d\n", x);
+
+    printf("Number after triple left shift is %d\n", n);
+
+    printf("Number after double right shift is %d\n", m);
+}
+
+int XOR (char a, char b)
+{
+    if (a != 0)
+    {
+        a = 1;
+    }
+
+    if (b != 0)
+    {
+        b = 1;
+    }
+
+    char c = a ^ b;
+
+    printf("XOR of these variables can be 1 - true or 0 - false. Result: %d\n", c);
+
+    return 0;
+}
+
+void PrintBites(int x)
+{
+    int firstLoopVar = x;
+
+    int cellCounter = 0;
+
+    while (firstLoopVar > 0)
+    {
+        firstLoopVar = firstLoopVar / 2;
+
+        cellCounter++;
+    }
+
+    int Binary[cellCounter];
+
+    int unit;
+
+    while (x > 0)
+    {
+        unit = x % 2;
+
+        x = x / 2;
+
+        Binary[cellCounter - 1] = unit;
+
+        cellCounter--;
+    }
+
+    printf("Your number in binary is: ");
+
+    for (int i = 0; i < sizeof(Binary) / sizeof (Binary[0]); i++)
+    {
+        printf("%d", Binary[i]);
+    }
+
+    printf("\n");
+}
+
 int main()
 {
-    FirstMethod(Thirteen);
+    FirstMethod(101);
 
     DisplayTypeSize();
 
-    int result;
+    int result = Compare(5,5);
 
-    result = Compare(5,5);
+    printf("a == b = 0, a > b = 1, a < b = -1. Result: %d\n", result);
 
-    printf("%d\n", result);
+    MoveBites();
+
+    XOR(-127,0);
+
+    PrintBites(12456);
 
     return 0;
 }
